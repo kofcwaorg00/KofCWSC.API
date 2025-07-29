6be02b207540dc42a5b4060da1b620df2fa2e1d0
Tim Philomeno <tphilomeno@kofc-wa.org>
2025-07-29 10:44:46 -0400

    added MI

--- KofCWSC.API/Models/DuplicateMember.cs
+++ KofCWSC.API/Models/DuplicateMember.cs
@@ -3,6 +3,7 @@
     public partial class DuplicateMember
     {
         public string FirstName { get; set; }
+        public string? MI {  get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public int Number {  get; set; }
